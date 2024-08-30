77f727663df61524ba0ff042248d847d13d18667
CarlosMayo1 <carloshmayo@gmail.com>
2024-08-29 22:03:07 -0500

    adding filters to the product by container section

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,18 +1,395 @@
 import { useState, useEffect } from 'react'
 import { supabase } from '../utils/supabase.client'
+import { useForm } from 'react-hook-form'
 
 // Supabase
 const fetchProducts = async () => {
 	const { error, data } = await supabase
-		.from('products')
+		.from('product_container')
 		.select(
-			'productId, productName, productAmount, productExpirationDate, productObservations, productCategory(productCategoryId, productCategoryName), productType(productTypeId, productTypeName), productBrand(productBrandId, productBrandName)',
+			'productContainerId, productId, products:productId(productName, productExpirationDate, productBrand:productBrandId(productBrandId, productBrandName), productType:productTypeId(productTypeId, productTypeName), productObservations), containers:containerId(containerId, containerCode), productContainerAmount',
 		)
+	return data
+}
+
+const fetchFilteredProductsByName = async productName => {
+	const { error, data } = await supabase
+		.from('product_container')
+		.select(
+			'productContainerId, productId, products:productId(productName, productExpirationDate, productBrand:productBrandId(productBrandId, productBrandName), productType:productTypeId(productTypeId, productTypeName), productObservations), containers:containerId(containerId, containerCode), productContainerAmount',
+		)
+		.ilike('products.productName', `%${productName}%`)
+	// .eq('containerId', containerId)
+
+	return data
+}
+
+const fetchFilteredProductsByCode = async containerId => {
+	const { error, data } = await supabase
+		.from('product_container')
+		.select(
+			'productContainerId, productId, products:productId(productName, productExpirationDate, productBrand:productBrandId(productBrandId, productBrandName), productType:productTypeId(productTypeId, productTypeName), productObservations), containers:containerId(containerId, containerCode), productContainerAmount',
+		)
+		.eq('containerId', containerId)
+
+	return data
+}
+
+const fetchFilteredProductsByDate = async date => {
+	const { error, data } = await supabase
+		.from('product_container')
+		.select(
+			'productContainerId, productId, products:productId(productName, productExpirationDate, productBrand:productBrandId(productBrandId, productBrandName), productType:productTypeId(productTypeId, productTypeName), productObservations), containers:containerId(containerId, containerCode), productContainerAmount',
+		)
+		.lte('products.productExpirationDate', date)
+
+	return data
+}
+
+const fetchFilteredProductsByAmount = async amount => {
+	const { error, data } = await supabase
+		.from('product_container')
+		.select(
+			'productContainerId, productId, products:productId(productName, productExpirationDate, productBrand:productBrandId(productBrandId, productBrandName), productType:productTypeId(productTypeId, productTypeName), productObservations), containers:containerId(containerId, containerCode), productContainerAmount',
+		)
+		.lte('productContainerAmount', amount)
+
+	return data
+}
+
+const fetchFilteredProductsByCodeAndName = async (containerId, productName) => {
+	const { error, data } = await supabase
+		.from('product_container')
+		.select(
+			'productContainerId, productId, products:productId(productName, productExpirationDate, productBrand:productBrandId(productBrandId, productBrandName), productType:productTypeId(productTypeId, productTypeName), productObservations), containers:containerId(containerId, containerCode), productContainerAmount',
+		)
+		.eq('containerId', containerId)
+		.ilike('products.productName', `%${productName}%`)
+
+	return data
+}
+
+const fetchFilteredProductsByCodeAndDate = async (
+	containerId,
+	productExpirationDate,
+) => {
+	const { error, data } = await supabase
+		.from('product_container')
+		.select(
+			'productContainerId, productId, products:productId(productName, productExpirationDate, productBrand:productBrandId(productBrandId, productBrandName), productType:productTypeId(productTypeId, productTypeName), productObservations), containers:containerId(containerId, containerCode), productContainerAmount',
+		)
+		.eq('containerId', containerId)
+		.lte('products.productExpirationDate', productExpirationDate)
+
+	return data
+}
+
+const fetchFilteredProductsByCodeAndAmount = async (
+	containerId,
+	productContainerAmount,
+) => {
+	const { error, data } = await supabase
+		.from('product_container')
+		.select(
+			'productContainerId, productId, products:productId(productName, productExpirationDate, productBrand:productBrandId(productBrandId, productBrandName), productType:productTypeId(productTypeId, productTypeName), productObservations), containers:containerId(containerId, containerCode), productContainerAmount',
+		)
+		.eq('containerId', containerId)
+		.lte('productContainerAmount', productContainerAmount)
+
+	return data
+}
+
+const fetchFilteredProductsByNameAndDate = async (
+	productName,
+	productExpirationDate,
+) => {
+	const { error, data } = await supabase
+		.from('product_container')
+		.select(
+			'productContainerId, productId, products:productId(productName, productExpirationDate, productBrand:productBrandId(productBrandId, productBrandName), productType:productTypeId(productTypeId, productTypeName), productObservations), containers:containerId(containerId, containerCode), productContainerAmount',
+		)
+		.ilike('products.productName', `%${productName}%`)
+		.lte('products.productExpirationDate', productExpirationDate)
+
+	return data
+}
+
+const fetchFilteredProductsByNameAndAmount = async (
+	productName,
+	productContainerAmount,
+) => {
+	const { error, data } = await supabase
+		.from('product_container')
+		.select(
+			'productContainerId, productId, products:productId(productName, productExpirationDate, productBrand:productBrandId(productBrandId, productBrandName), productType:productTypeId(productTypeId, productTypeName), productObservations), containers:containerId(containerId, containerCode), productContainerAmount',
+		)
+		.ilike('products.productName', `%${productName}%`)
+		.lte('productContainerAmount', productContainerAmount)
+
+	return data
+}
+
+const fetchFilteredProductsByNameDateAndAmount = async (
+	productName,
+	productExpirationDate,
+	productContainerAmount,
+) => {
+	const { error, data } = await supabase
+		.from('product_container')
+		.select(
+			'productContainerId, productId, products:productId(productName, productExpirationDate, productBrand:productBrandId(productBrandId, productBrandName), productType:productTypeId(productTypeId, productTypeName), productObservations), containers:containerId(containerId, containerCode), productContainerAmount',
+		)
+		.ilike('products.productName', `%${productName}%`)
+		.lte('products.productExpirationDate', productExpirationDate)
+		.lte('productContainerAmount', productContainerAmount)
+
+	return data
+}
+
+const fetchFilteredProductsByDateAndAmount = async (
+	productExpirationDate,
+	productContainerAmount,
+) => {
+	const { error, data } = await supabase
+		.from('product_container')
+		.select(
+			'productContainerId, productId, products:productId(productName, productExpirationDate, productBrand:productBrandId(productBrandId, productBrandName), productType:productTypeId(productTypeId, productTypeName), productObservations), containers:containerId(containerId, containerCode), productContainerAmount',
+		)
+		.lte('products.productExpirationDate', productExpirationDate)
+		.lte('productContainerAmount', productContainerAmount)
+
+	return data
+}
+
+const fetchFilteredProductsByAllFilters = async (
+	productContainerCode,
+	productName,
+	productExpirationDate,
+	productContainerAmount,
+) => {
+	const { error, data } = await supabase
+		.from('product_container')
+		.select(
+			'productContainerId, productId, products:productId(productName, productExpirationDate, productBrand:productBrandId(productBrandId, productBrandName), productType:productTypeId(productTypeId, productTypeName), productObservations), containers:containerId(containerId, containerCode), productContainerAmount',
+		)
+		.eq('containerId', productContainerCode)
+		.ilike('products.productName', `%${productName}%`)
+		.lte('products.productExpirationDate', productExpirationDate)
+		.lte('productContainerAmount', productContainerAmount)
+
 	return data
 }
 
 const Products = () => {
 	const [products, setProducts] = useState([])
+	const { handleSubmit, register } = useForm()
+
+	const onFilteredProductsHandler = handleSubmit(data => {
+		// fiilter by container code
+		if (
+			data.filterByCode !== '' &&
+			data.filterByName === '' &&
+			data.filterByAmount === '' &&
+			data.filterByDate === ''
+		) {
+			fetchFilteredProductsByCode(data.filterByCode).then(response =>
+				console.log(response),
+			)
+		}
+
+		// filter by name of the product
+		if (
+			data.filterByCode === '' &&
+			data.filterByName !== '' &&
+			data.filterByAmount === '' &&
+			data.filterByDate === ''
+		) {
+			fetchFilteredProductsByName(data.filterByName).then(response => {
+				// avoids fetching null products
+				const listOfFilteredProducts = response.filter(
+					product => product.products !== null,
+				)
+				console.log(listOfFilteredProducts)
+			})
+		}
+
+		// filter by date
+		if (
+			data.filterByCode === '' &&
+			data.filterByName === '' &&
+			data.filterByDate !== '' &&
+			data.filterByAmount === ''
+		) {
+			fetchFilteredProductsByDate(data.filterByDate).then(response => {
+				// avoids fetching null products
+				const listOfFilteredProducts = response.filter(
+					product => product.products !== null,
+				)
+				console.log(listOfFilteredProducts)
+			})
+		}
+
+		// filter by amount
+		if (
+			data.filterByCode === '' &&
+			data.filterByName === '' &&
+			data.filterByDate === '' &&
+			data.filterByAmount !== ''
+		) {
+			fetchFilteredProductsByAmount(data.filterByAmount).then(response => {
+				console.log(response)
+			})
+		}
+
+		// filter by code and product name
+		if (
+			data.filterByCode !== '' &&
+			data.filterByName !== '' &&
+			data.filterByAmount === '' &&
+			data.filterByDate === ''
+		) {
+			fetchFilteredProductsByCodeAndName(
+				data.filterByCode,
+				data.filterByName,
+			).then(response => {
+				// avoids fetching null products
+				const filteredListOfProducts = response.filter(
+					product => product.products !== null,
+				)
+				console.log(filteredListOfProducts)
+			})
+		}
+
+		// filter by code and date
+		if (
+			data.filterByCode !== '' &&
+			data.filterByDate !== '' &&
+			data.filterByName === '' &&
+			data.filterByAmount === ''
+		) {
+			fetchFilteredProductsByCodeAndDate(
+				data.filterByCode,
+				data.filterByDate,
+			).then(response => {
+				// console.log(response)
+				const listOfFilteredProducts = response.filter(
+					product => product.products !== null,
+				)
+				console.log(listOfFilteredProducts)
+			})
+		}
+
+		// filter by code and amount
+		if (
+			data.filterByCode !== '' &&
+			data.filterByAmount !== '' &&
+			data.filterByName === '' &&
+			data.filterByDate === ''
+		) {
+			fetchFilteredProductsByCodeAndAmount(
+				data.filterByCode,
+				data.filterByAmount,
+			).then(response => {
+				const listOfFilteredProducts = response.filter(
+					product => product.products !== null,
+				)
+				console.log(listOfFilteredProducts)
+			})
+		}
+
+		// filter by name and date
+		if (
+			data.filterByName !== '' &&
+			data.filterByDate !== '' &&
+			data.filterByAmount === '' &&
+			data.filterByCode === ''
+		) {
+			fetchFilteredProductsByNameAndDate(
+				data.filterByName,
+				data.filterByDate,
+			).then(response => {
+				const listOfFilteredProducts = response.filter(
+					product => product.products !== null,
+				)
+				console.log(listOfFilteredProducts)
+			})
+		}
+
+		// filter by name and amount
+		if (
+			data.filterByName !== '' &&
+			data.filterByAmount !== '' &&
+			data.filterByCode === '' &&
+			data.filterByDate === ''
+		) {
+			fetchFilteredProductsByNameAndAmount(
+				data.filterByName,
+				data.filterByAmount,
+			).then(response => {
+				const listOfFilteredProducts = response.filter(
+					product => product.products !== null,
+				)
+				console.log(listOfFilteredProducts)
+			})
+		}
+
+		// filter by name, date and amount
+		if (
+			data.filterByName !== '' &&
+			data.filterByAmount !== '' &&
+			data.filterByDate !== '' &&
+			data.filterByCode === ''
+		) {
+			fetchFilteredProductsByNameDateAndAmount(
+				data.filterByName,
+				data.filterByDate,
+				data.filterByAmount,
+			).then(response => {
+				const listOfFilteredProducts = response.filter(
+					product => product.products !== null,
+				)
+				console.log(listOfFilteredProducts)
+			})
+		}
+
+		// filter by date and amount
+		if (
+			data.filterByDate !== '' &&
+			data.filterByAmount !== '' &&
+			data.filterByCode === '' &&
+			data.filterByName === ''
+		) {
+			fetchFilteredProductsByDateAndAmount(
+				data.filterByDate,
+				data.filterByAmount,
+			).then(response => {
+				const listOfFilteredProducts = response.filter(
+					product => product.products !== null,
+				)
+				console.log(listOfFilteredProducts)
+			})
+		}
+
+		// filter by all filters
+		if (
+			data.filterByCode !== '' &&
+			data.filterByName !== '' &&
+			data.filterByDate !== '' &&
+			data.filterByAmount !== ''
+		) {
+			fetchFilteredProductsByAllFilters(
+				data.filterByCode,
+				data.filterByName,
+				data.filterByDate,
+				data.filterByAmount,
+			).then(response => {
+				const filteredProducts = response.filter(
+					product => product.products !== null,
+				)
+				console.log(filteredProducts)
+			})
+		}
+	})
 
 	useEffect(() => {
 		fetchProducts().then(response => {
@@ -24,41 +401,100 @@ const Products = () => {
 	return (
 		<section>
 			<h1 className='text-lg py-2 font-bold'>Lista de productos</h1>
-			<div>
+			<div className='mb-2'>
 				<p>Cantidad de productos totales 21</p>
 				<p>Productos pendientes por almacenar 4</p>
+			</div>
+			<div className='mb-2 border border-black p-2'>
+				<form onSubmit={onFilteredProductsHandler}>
+					<h2 className='font-bold text-lg'>Filtros</h2>
+					<div className='flex mb-2'>
+						<label id='filterByCode'>Filtrar por Codigo</label>
+						<select
+							className='border border-black'
+							{...register('filterByCode')}
+						>
+							<option value=''>Seleccione el codigo</option>
+							<option value='ffc5bfdb-786a-41f8-9500-f8d6dea4c606'>
+								DN000001
+							</option>
+							<option value='eab40a52-617e-498b-8b94-aafb2fd13aa3'>
+								DN000002
+							</option>
+						</select>
+					</div>
+					<div className='mb-2'>
+						<label>Filtrar por nombre</label>
+						<input
+							type='text'
+							className='border border-black'
+							{...register('filterByName')}
+						/>
+					</div>
+					<div className='mb-2'>
+						<label>Filtrar por fecha</label>
+						<input
+							type='date'
+							className='border border-black'
+							{...register('filterByDate')}
+						/>
+					</div>
+					<div>
+						<label>Filtrar por cantidad</label>
+						<input
+							type='number'
+							className='border border-black'
+							{...register('filterByAmount')}
+						/>
+					</div>
+					<div>
+						<button
+							type='submit'
+							className='bg-blue-400 text-white font-bold px-2 py-2 rounded-md'
+						>
+							Filtrar
+						</button>
+					</div>
+				</form>
 			</div>
 			<table className='border border-black'>
 				<thead>
 					<tr>
 						<th className='border border-black'>Nombre</th>
-						<th className='border border-black'>Categoría</th>
 						<th className='border border-black'>Tipo</th>
-						<th className='border border-black'>Marca</th>
 						<th className='border border-black'>Cantidad</th>
-						<th className='border border-black'>Fecha de vencimiento</th>
+						<th className='border border-black'>Vencimiento</th>
 						<th className='border border-black'>Observaciones</th>
+						<th className='border border-black'>Contenedor</th>
+						<th className='border border-black'>Acción</th>
 					</tr>
 				</thead>
 				<tbody>
 					{products.map(product => (
-						<tr key={product.productId}>
-							<td className='border border-black'>{product.productName}</td>
+						<tr key={product.productContainerId}>
 							<td className='border border-black'>
-								{product.productCategory.productCategoryName}
+								{product.products.productName}
 							</td>
 							<td className='border border-black'>
-								{product.productType.productTypeName}
+								{product.products.productType.productTypeName}
 							</td>
 							<td className='border border-black'>
-								{product.productBrand.productBrandName}
-							</td>
-							<td className='border border-black'>{product.productAmount}</td>
-							<td className='border border-black'>
-								{product.productExpirationDate}
+								{product.productContainerAmount}
 							</td>
 							<td className='border border-black'>
-								{product.productObservations}
+								{product.products.productExpirationDate}
+							</td>
+							<td className='border border-black'>
+								{product.products.productObservations}
+							</td>
+							<td className='border border-black'>
+								{product.containers.containerCode}
+							</td>
+							<td className='border border-black'>
+								<div>
+									<button>Mover</button>
+									{/* <button>Eliminar</button> */}
+								</div>
 							</td>
 						</tr>
 					))}
